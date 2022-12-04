015e21bf6af69eb8d0012f7ed68693c9e8b658f2
Amol shelke <shelkeamol897@gmail.com>
2022-12-04 23:05:39 +0530

    Set the isLogged value in localStorage to not log in again and again and use the useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,14 +7,27 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    // getting that isLoggedIn localStorage value
+    const isLoggedInInfo = localStorage.getItem('isLoggedIn');
+
+    // creating a condition where if the user logged in once and reload the page he/she will always be logged in
+    if (isLoggedInInfo) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // Setting the isLoggedIn state true inside the localStorage.
     localStorage.setItem('isLoggedIn', 'true');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
