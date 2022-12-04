99d9ac99f3d659e8b486705ee1a44a5cca5e83c4
Amol shelke <shelkeamol897@gmail.com>
2022-12-05 00:12:04 +0530

    Added useState for the email valid handler

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -13,18 +13,14 @@ const Login = props => {
 
   const emailChangeHandler = event => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6,
-    );
   };
+
+  useEffect(() => {
+    setFormIsValid(enteredEmail.includes('@') && enteredPassword.length > 6);
+  }, [enteredEmail, enteredPassword]);
 
   const passwordChangeHandler = event => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@'),
-    );
   };
 
   const validateEmailHandler = () => {
